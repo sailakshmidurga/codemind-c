b484c79f8c8d9f80430b321524f3ca3ee527cb35
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2023-11-11 10:19:06 +0530

    #include<stdio.h> int main() {     int n,t,rev=0,r;     scanf("%d",&n);     t=n;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     if(t==rev)     {         printf("Palindrome");     }     else     {         printf("Not Palindrome");     }
    }

---
+++ Palindrome.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,t,rev=0,r;
+    scanf("%d",&n);
+    t=n;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if(t==rev)
+    {
+        printf("Palindrome");
+    }
+    else
+    {
+        printf("Not Palindrome");
+    }
+    
+}
