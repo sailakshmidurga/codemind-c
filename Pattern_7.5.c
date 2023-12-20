145fda746c50a592b86c182905c75aec2339a8fe
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2023-12-20 10:48:18 +0530

    #include<stdio.h> int main() {     int i,j,k,n,count='A';     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(k=1;k<=n-i;k++)         {             printf(" ");         }         for(j=1;j<=i;j++)         {             printf("%c ",count);         }         count++;         printf(" ");     } }

---
+++ Pattern_7.5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n,count='A';
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",count);
+        }
+        count++;
+        printf("
+");
+    }
+}
