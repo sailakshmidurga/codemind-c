e8ce9820106da79615b4267ef57477de579db8c9
sailakshmidurga <147791796+sailakshmidurga@users.noreply.github.com>
2024-01-21 15:26:58 +0530

    #include<stdio.h> int main() {     int b,c,d,x,y,e=0,k,i,j;     scanf("%d",&b);     int a[b];     for(i=0;i<b;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&x,&y);     for(j=0;j<b;j++)     {         if(a[j]>=x && a[j]<=y)         {             continue;         }         e=e+a[j];     }     printf("%d",e); }

---
+++ Sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int b,c,d,x,y,e=0,k,i,j;
+    scanf("%d",&b);
+    int a[b];
+    for(i=0;i<b;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&x,&y);
+    for(j=0;j<b;j++)
+    {
+        if(a[j]>=x && a[j]<=y)
+        {
+            continue;
+        }
+        e=e+a[j];
+    }
+    printf("%d",e);
+}
